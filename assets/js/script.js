9ccc7a0930e5395e75efb1423dd3061a2ffcdba7
Natasha Ortiz <tashaaortiz@gmail.com>
2021-08-21 23:20:40 -0500

    uploading hmwk files

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const theQs = [
+{
+    question: "What does HTML stand for?",
+    choices: {
+        a: "How True My Lady",
+        b: "Hyper Text Markup Language",
+        c: "Hyper Text Mask Layout",
+    },
+    correctChoice: "b"
+},
+{
+    question: "What does CSS stand for?",
+    choices: {
+        a: "Cool Setup Seth",
+        b: "Cows See Soup",
+        c: "Cascading Style Sheet"
+    },
+correctChoice: "c"
+},
+{
+    question: "JavaScript is closely linked to which Web API?",
+    choices: {
+        a: "Bootstrap",
+        b: "Google Fonts",
+        c: "JQuery"
+    },
+    correctChoice: "c"
+}
+];
